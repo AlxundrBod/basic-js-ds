87881d79bc89b8785031fb848800680d181e2399
AlxundrBod <talismanariona@gmail.com>
2023-04-17 16:58:59 +0700

    feat: stack and queue complete

--- src/stack.js
+++ src/stack.js
@@ -13,18 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.data = [];
   }
 
+  push(element) {
+
+    this.data.push(element);
+    // remove line with error and write your code here
+  }
   pop() {
-    throw new NotImplementedError('Not implemented');
+    return this.data.pop();
     // remove line with error and write your code here
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    return this.data[this.data.length - 1];
     // remove line with error and write your code here
   }
 }
